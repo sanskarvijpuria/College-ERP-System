5f1059fc0ce86843baec28e62e624838fd30c03d
Sanskar Vijpuria <sanskarvijpuria@users.noreply.github.com>
2021-06-29 17:29:21 +0530

    Updating wsgi and requirement

--- student_management_system/wsgi.py
+++ student_management_system/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_management_system.settings')
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
